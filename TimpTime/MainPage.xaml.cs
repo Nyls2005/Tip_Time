fb6171fd264da854b1446258e492558ea31ba320
Jônatas <jolimenezes@gmail.com>
2025-09-08 22:32:35 -0300

    Try e catch, e arredondamento
    Abrangendo erros com try e catch, e logica dos botões de arredondamento

--- TimpTime/MainPage.xaml.cs
+++ TimpTime/MainPage.xaml.cs
@@ -4,19 +4,22 @@
     {
         int count = 0;
 
+        
+
         public MainPage()
         {
             InitializeComponent();
+            PorcentagemSlider.Value = 15;
         }
 
         private void ArredondaCimaBtn_Clicked(object sender, EventArgs e)
         {
-
+            Math.Ceiling(Convert.ToDouble(ValorTotalLabel));
         }
 
         private void ArredondaBaixoBtn_Clicked(object sender, EventArgs e)
         {
-
+            Math.Floor(Convert.ToDouble(ValorTotalLabel));
         }
 
         private void Porcentagem20Btn_Clicked(object sender, EventArgs e)
@@ -26,21 +29,32 @@
 
         private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            float Porcentag = (float)PorcentagemSlider.Value;
-            float ValorTotal = (float.Parse(ValorTotalEntry.Text));
-            
-            float ValorGorgeta = ValorTotal * (Porcentag / 100);
-            
-            ValorGorjetaLabel.Text = ValorGorgeta.ToString();
+                try
+                {
 
-            PorcentagemGorjetaLabel.Text = Porcentag.ToString() + '%';
+                    float Porcentag = (float)PorcentagemSlider.Value;
+                    float ValorTotal = (float.Parse(ValorTotalEntry.Text));
 
-            float ValorFinal = ValorTotal + ValorGorgeta;
+                    float ValorGorgeta = ValorTotal * (Porcentag / 100);
 
-            ValorTotalLabel.Text = ValorFinal.ToString();
+                    ValorGorjetaLabel.Text = ValorGorgeta.ToString("C");
 
+                    PorcentagemGorjetaLabel.Text = Porcentag.ToString("F") + '%';
 
-        }
+                    float ValorFinal = ValorTotal + ValorGorgeta;
+
+                    ValorTotalLabel.Text = ValorFinal.ToString("C");
+
+                    ErroLabel.Text = "";
+                } 
+                catch (Exception r)
+                {
+                    Console.WriteLine(r.ToString());
+                    ErroLabel.Text = "Por favor, insira um valor válido.";
+            }
+
+            }
+
 
         private void Porcentagem15Btn_Clicked(object sender, EventArgs e)
         {
